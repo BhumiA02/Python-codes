7ce7a79d59383b8658de45b2c1e00fc5d146261d
Bhumi <bhumiarora530@gmail.com>
2024-11-24 00:36:42 +0530

    Delete program2.py

--- program2.py
+++
@@ -1,7 +0,0 @@
-name="bhumi"#name=name of variable,bhumi=value of variable
-age=20
-print(name)
-print("my name is:",name)
-#imp rule:we can not use %,!,@,#,$ these symbols in the name of variables
-#:we can not start with digit like "1age"
-#it can be of any length
