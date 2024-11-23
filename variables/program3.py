2724ec58be68b8921d5fa820d14d2da5ffd7ca32
Bhumi <bhumiarora530@gmail.com>
2024-11-24 00:46:55 +0530

    this folder contains programs related to variables

---
+++ variables/program3.py
@@ -0,0 +1,6 @@
+name="bhumi"
+age=20
+print(type(name))
+print(type(age))
+#name is in string and age is in integer(string,int,float,boolean,none are data types)
+#boolean includes True,False,None(first letter of these keywords must be capital because python is case sensitive lang)
---
+++ variables/program4.py
@@ -0,0 +1,13 @@
+a=4
+b=2
+sum=a+b
+diff=a-b
+print(sum)
+print(diff)
+"""
+print(sum)
+print(diff)print(sum)
+print(diff)print(sum)
+print(diff)
+"""
+#"""we can use this for comments thwe multiple lines
